d63171d999b271793a19080045b1a6092c365f77
theAkorede <koredeaa77@gmail.com>
2025-11-05 05:01:26 +0100

    fix coursepage

--- app/(user)/search/page.tsx
+++ app/(user)/search/page.tsx
@@ -36,7 +36,7 @@ export default async function SearchPage({ params }: SearchPageProps) {
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {courses.map((course) => (
+            {courses.map((course: any) => (
               <CourseCard
                 key={course._id}
                 course={course}
